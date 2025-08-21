7ad4c678b64f4aa0afcb0ac9abfbeee3f393aaab
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-21 10:26:26 -0300

    Create methods to db

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,40 @@
 import express, { Request, Response } from "express"
+import { knex } from "./database/knex"
 
 const app = express()
 app.use(express.json())
 
-app.get("/", async (request: Request, response: Response) => {
-  response.json({ message: "Hello World!" })
+app.post("/courses", async (request: Request, response: Response) => {
+  const { name } = request.body
+
+  await knex("courses").insert({ name })
+  // await knex.raw("INSERT INTO courses (name) VALUES (?)", [name])
+
+  return response.status(201).json()
+})
+
+app.get("/courses", async (request: Request, response: Response) => {
+  // const courses = await knex.raw("SELECT * FROM courses")
+  const courses = await knex("courses").select().orderBy("name")
+
+  return response.json(courses)
+})
+
+app.put("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params
+  const { name } = request.body
+
+  await knex("courses").update({ name }).where({ id })
+
+  return response.json()
+})
+
+app.delete("/courses/:id", async (request: Request, response: Response) => {
+  const { id } = request.params
+
+  await knex("courses").delete().where({ id })
+
+  return response.json()
 })
 
 app.listen(3333, () => console.log(`Server is running on port 3333`))
